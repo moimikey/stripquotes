3d31615510bd7b3357047a2a1c490ae0e1c09626
Michael Scott Hertzberg <mhertzberg@shopkeep.com>
2015-10-23 17:04:54 -0400

    continue working on this...

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-/**
- * strip initial and ending single/double quotes
- * from a single/multiline string
- *
- * @param  {String} q
- * @return {String} s
- */
-module.exports = function stripquotes(q) {
-  var s = q
-  var t = s.length
-  if (s.charAt(0).match(/\u0022|\u0027/))   s = s.substring(1, t--)
-  if (s.charAt(--t).match(/\u0022|\u0027/)) s = s.substring(0, t)
-  return s
-}
+module.exports = require('./lib')
--- test.js
+++ test.js
@@ -1,25 +1,25 @@
-var test = require('tape');
+var test = require('tape')
 
 test('returns a `stripquotes` function', function (t) {
   var stripquotes = require('./')
   t.plan(1)
   t.equal(typeof stripquotes, 'function')
   t.end()
-});
+})
 
 test('can remove single quotes from a single line string', function (t) {
   var stripquotes = require('./')
   t.plan(1)
   t.equal(stripquotes("'cheese crackers'"), 'cheese crackers')
   t.end()
-});
+})
 
 test('can remove double quotes from a single line string', function (t) {
   var stripquotes = require('./')
   t.plan(1)
   t.equal(stripquotes('"cheese crackers"'), 'cheese crackers')
   t.end()
-});
+})
 
 test('can remove single quotes from a multiline string', function (t) {
   var stripquotes = require('./')
@@ -30,7 +30,7 @@ test('can remove single quotes from a multiline string', function (t) {
                                \
           crackers')
   t.end()
-});
+})
 
 test('can remove double quotes from a multiline string', function (t) {
   var stripquotes = require('./')
@@ -41,4 +41,54 @@ test('can remove double quotes from a multiline string', function (t) {
                                \
           crackers')
   t.end()
-});
+})
+
+test('will throw if a non-string is passed to the constructor', function (t) {
+  var stripquotes = require('./')
+  t.plan(3)
+  t.throws(function() {
+    stripquotes({})
+  });
+  t.throws(function() {
+    stripquotes()
+  });
+  t.throws(function() {
+    stripquotes(true)
+  });
+  t.end()
+})
+
+test('can remove single curly/smart quotes from a string', function (t) {
+  var stripquotes = require('./')
+  t.plan(1)
+  t.equal(stripquotes('‘cheese crackers’'), 'cheese crackers')
+  t.end()
+})
+
+test('can remove double curly/smart quotes from a string', function (t) {
+  var stripquotes = require('./')
+  t.plan(1)
+  t.equal(stripquotes('“cheese crackers”'), 'cheese crackers')
+  t.end()
+})
+
+test('can remove single guillemets from a string', function (t) {
+  var stripquotes = require('./')
+  t.plan(1)
+  t.equal(stripquotes('‹cheese crackers›'), 'cheese crackers')
+  t.end()
+})
+
+test('can remove double guillemets from a string', function (t) {
+  var stripquotes = require('./')
+  t.plan(1)
+  t.equal(stripquotes('«cheese crackers»'), 'cheese crackers')
+  t.end()
+})
+
+test.skip('this should fail', function (t) {
+  var stripquotes = require('./')
+  t.plan(1)
+  t.equal(stripquotes('«cheese crackers›'), 'cheese crackers')
+  t.end()
+})
